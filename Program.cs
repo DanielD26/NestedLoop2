b4efbae9fb5ce420c9e0d3616c80d027385f1caa
DanielD26 <103547416@student.swin.edu.au>
2021-04-15 22:50:26 +1000

    first commit

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace NestedLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string name = Console.ReadLine();
+            // int counter = 0;
+
+            // while (counter < 10) {
+    
+
+            //     int counter2 = counter;
+
+            //     while (counter2 < 5) {
+            //         Console.Write(name);
+            //         counter2 = counter2 + 1;
+            //     }
+            //     Console.WriteLine();
+            //     counter = counter + 1;
+            // }
+
+            // Console.WriteLine("Goodbye "+ name);
+
+            // int counter = 0;
+            
+            // while (counter < 10) {
+            //     int counter2 = counter;
+
+            //     while (counter2 < 5) {
+            //         Console.Write('x');
+            //         counter2++;
+            //     }
+
+            //     Console.WriteLine();
+            //     counter++;
+            // }
+
+            // Console.WriteLine("Enter how many X's to start: ");
+            // string getStart = Console.ReadLine();
+            // int start = int.Parse(getStart);
+
+            // int counter = 0;
+            // while(counter < start*2) {
+            //     int counter2 = counter;
+
+            //     while(counter2 < start) {
+            //         Console.Write('x');
+            //         counter2++;
+            //     }
+
+            //     Console.WriteLine();
+            //     counter++;
+            // }
+
+            // int counter = 0;
+            // while (counter < 10) {
+            //     int counter2 = 0;
+            //     counter++;
+            //     while (counter2 < counter) {
+            //         Console.Write('x');
+            //         counter2++;
+            //     }
+            //     Console.WriteLine();
+            // }
+
+            Console.Write("Enter how many lines of X's you want: ");
+            string getLines = Console.ReadLine();
+            int lines = int.Parse(getLines);
+
+            int counter = 0;
+            while (counter < lines) {
+                int counter2 = 0;
+                counter++;
+                while (counter2 < counter) {
+                    Console.Write('x');
+                    counter2++;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
